6893d9990f9579532edb4d02f14ba587716d15ce
Veljko Popovic <v.s.popovic@gmail.com>
2016-08-18 16:59:33 +0200

    following the latest node module

--- dist/moneystringmanipulation.js
+++ dist/moneystringmanipulation.js
@@ -16,7 +16,7 @@ function createLib(isString, isNumber, prependToString, isInteger) {
     }
 
     if (isNumber(str)) {
-      return (isInteger(str)) ? str*100 : ~~(str*100);
+      return ~~(str*power);
     }
 
     if (!isString(str)) {
