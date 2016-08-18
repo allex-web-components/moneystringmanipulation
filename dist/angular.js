277d82bd3a84b37b9d6f8fa2cb4773a1228949bb
Veljko Popovic <v.s.popovic@gmail.com>
2016-08-18 10:29:55 +0200

    moving along

--- dist/angular.js
+++ dist/angular.js
@@ -4,18 +4,19 @@ angular.module ('allex__web_moneystringmanipulationcomponent', []);
   'use strict';
 
 
-  module.filter ('allex_moneystringmanipulation.toMoney', function () {
+  module.filter ('allex_moneystringmanipulation_toMoney', function () {
     return function (input, decimals) {
-      if (!allexlib.isNumber(decimals)) throw new Error('Decimals must be a number'); ///TODO: not just a number, non negative integer as well ...
+      if (!allexlib.isInteger(decimals)) throw new Error('Decimals must be a number');
       return moneystringmanipulation.toMoney(input, decimals);
     };
   });
 
 
-  module.filter('allex_moneystringmanipulation.fromMoney', function () {
+  module.filter('allex_moneystringmanipulation_fromMoney', function () {
     return function (input, decimals) {
-      if (!allexlib.isNumber(decimals)) throw new Error('decimals must be a number');
-      return moneystringmanipulation.fromMoney (input, decimals);
+      if (!allexlib.isNumber(decimals)) throw new Error('decimals must be a number: '+decimals);
+      if (!allexlib.isInteger(input)) throw new Error('input is not a number: '+input);
+      return moneystringmanipulation.fromMoney (parseInt (input, 10), decimals);
     };
   });
 
--- dist/moneystringmanipulation.js
+++ dist/moneystringmanipulation.js
@@ -1,6 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function createLib(isString, isNumber, prependToString) {
   'use strict';
+  //TODO: introduce a bit more strict checks on input params and possible conversions string <-> integer ...
 
   function toMoneyError(str, decimalplaces) {
     return new Error('Money string '+str+' has to be a positive decimal number with '+decimalplaces+' decimal places');
